b67c4245f6a11c9bacca83caa4accb2de98eb930
Miguel Escalona - DLSU <71485171+jm55DLSU@users.noreply.github.com>
2023-11-02 14:01:08 +0800

    Updated Lab2 Server

--- Lab2/4-11.py
+++ Lab2/4-11.py
@@ -2,7 +2,7 @@ import pika
 
 
 credentials = pika.PlainCredentials('rabbituser','rabbit1234')
-connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.5.39',5672,'/', credentials))
+connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.101.129',5672,'/', credentials))
 channel = connection.channel()
 
 
