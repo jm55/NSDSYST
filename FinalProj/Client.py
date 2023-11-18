6ec57ff52500320579fb92fbb15136af58c6336c
Miguel Escalona - DLSU <71485171+jm55DLSU@users.noreply.github.com>
2023-11-18 13:45:55 +0800

    Major Update
    Almost there...

--- FinalProj/Client.py
+++ FinalProj/Client.py
@@ -6,6 +6,7 @@ import cv2
 from multiprocessing import Lock, Process, Queue
 import pika
 from pika.exchange_type import ExchangeType
+from pika.exceptions import ChannelWrongStateError, ChannelClosedByBroker
 import uuid
 import datetime, time
 
@@ -21,48 +22,86 @@ class Client():
     PORT = 5672
     ROOT = '/'
 
-    received = Queue()
     def __init__(self):
         print("====CLIENT====")
         self.connect_to_server()
+        self.channel_rcv.basic_consume(queue = self.rcv_queue, on_message_callback=self.receive, auto_ack=True, exclusive=True)
+        self.consume = Process(target=self.channel_rcv.start_consuming, args=())
+        self.start_consuming()
+
+    def start_consuming(self):
+        self.consume.start()
+
+    def stop_consuming(self):
+        self.consume.join()
+        self.channel_rcv.stop_consuming()
+
+    def print_header(self):
+        return f"{datetime.datetime.now()}: Client - "
 
     def connect_to_server(self):
         self.CLIENT_UUID = str(uuid.uuid1())
         self.credentials = pika.PlainCredentials('rabbituser','rabbit1234')
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(Client.IP, Client.PORT, Client.ROOT, self.credentials, connection_attempts=128, retry_delay=1, heartbeat=600, blocked_connection_timeout=300))
-        print(f"{datetime.datetime.now()}: Client - Credentials -[{self.credentials.username}]:[{self.credentials.password}]")
-        self.channel = self.connection.channel()
-        self.channel.exchange_declare(exchange='adjustor', exchange_type=ExchangeType.direct)
-        # result = self.channel.queue_declare(queue='', exclusive=True)
-        # self.callback_queue = result.method.queue
-        # self.channel.basic_consume(
-        #     queue='adjustor',
-        #     on_message_callback=self.on_response,
-        #     auto_ack=True)
+        print(self.print_header() + f"Credentials -[{self.credentials.username}]:[{self.credentials.password}]")
+        print(self.print_header() + f"Connecting (rcv & snd) to RabbitMQ...")
+        self.connection_snd = pika.BlockingConnection(pika.ConnectionParameters(Client.IP, Client.PORT, Client.ROOT, self.credentials, connection_attempts=128, retry_delay=1, heartbeat=600, blocked_connection_timeout=300))
+        self.connection_rcv = pika.BlockingConnection(pika.ConnectionParameters(Client.IP, Client.PORT, Client.ROOT, self.credentials, connection_attempts=128, retry_delay=1, heartbeat=600, blocked_connection_timeout=300))
+        print(self.print_header() + f"Connected (rcv & snd) to RabbitMQ!")
+        print(self.print_header() + f"Creating Channels (rcv & snd)...")
+        self.channel_snd = self.connection_snd.channel()
+        self.channel_rcv = self.connection_rcv.channel()
+        print(self.print_header() + f"Channels Created (rcv & snd)!")
+        print(self.print_header() + f"Preparing Receiver...")
+        self.channel_rcv.exchange_declare(exchange='adjustor_fin', exchange_type=ExchangeType.topic)
+        result = self.channel_rcv.queue_declare(queue='', exclusive=True)
+        self.rcv_queue = result.method.queue
+        self.channel_rcv.queue_bind(exchange='adjustor_fin', queue=self.rcv_queue, routing_key=self.CLIENT_UUID)
+        print(self.print_header() + f"Receiver Prepared!")
         self.response = None
         self.corr_id = None
-    
-    def on_response(self, ch, method, props, body):
-        if self.corr_id == props.correlation_id:
-            self.response = body
+
+    def receive(self, ch, method, props, body):
+        json_obj = json.loads(body)
+        print(self.print_header() + f"Received {json_obj['filename']}")
+        if self.corr_id == props.correlation_id and json_obj['client_uid'] == self.CLIENT_UUID:
+            im = self.json2im(json_obj)
+            cv2.imwrite(json_obj["output"]+json_obj["filename"], im)
+            print(self.print_header() + f"File {json_obj['filename']} written!")
+        else:
+            print(self.print_header() + "Identity Mismatch Detected!")
     
     def send(self, json_str:str):
-        self.corr_id = str(uuid.uuid4())
-        self.channel.basic_publish(
-            exchange='',
-            routing_key='adjustor',
-            properties=pika.BasicProperties(
-                #reply_to=self.callback_queue,
-                correlation_id=self.corr_id,
-                headers={'client_uid':self.CLIENT_UUID, 'item_uid':self.corr_id},
-            ),
-            body=str(json_str),
-        )
-        self.connection.process_data_events()
+        try:
+            self.corr_id = str(uuid.uuid4())
+            self.channel_snd.basic_publish(
+                exchange='',
+                routing_key='adjustor',
+                properties=pika.BasicProperties(
+                    #reply_to=self.callback_queue,
+                    correlation_id=self.corr_id,
+                    headers={'client_uid':self.CLIENT_UUID, 'item_uid':self.corr_id},
+                ),
+                body=str(json_str),
+                mandatory=True
+            )
+            self.connection_snd.process_data_events()
+            return True
+        except ChannelWrongStateError as e:
+            print(self.print_header(), e.with_traceback(None))
+            print(self.print_header() + f"Restarting Connection & Channel...")
+            self.connection_snd = pika.BlockingConnection(pika.ConnectionParameters(Client.IP, Client.PORT, Client.ROOT, self.credentials, connection_attempts=128, retry_delay=1, heartbeat=600, blocked_connection_timeout=300))
+            self.channel_snd = self.connection_snd.channel()
+            return False
+        except ChannelClosedByBroker as e:
+            print(self.print_header(), e.with_traceback(None))
+            print(self.print_header() + f"Restarting Connection & Channel...")
+            self.connection_snd = pika.BlockingConnection(pika.ConnectionParameters(Client.IP, Client.PORT, Client.ROOT, self.credentials, connection_attempts=128, retry_delay=1, heartbeat=600, blocked_connection_timeout=300))
+            self.channel_snd = self.connection_snd.channel()
+            return False
     
     def create_folder(self, folderpath):
         if not os.path.exists(folderpath):
-            print(f"{datetime.datetime.now()}: Client - Making Folder...")
+            print(self.print_header() + f"Making Folder...")
             if platform == "linux" or platform == "linux2":
                 try:
                     original_umask = os.umask(0)
@@ -129,20 +168,36 @@ sharpness = 10
 c.create_folder(location_out)
 filenames = c.get_filenames(location_in)
 filenames.sort()
-print(f"{datetime.datetime.now()}: Client - Input Location = {location_in}")
-print(f"{datetime.datetime.now()}: Client - Output Location = {location_out}")
-print(f"{datetime.datetime.now()}: Client - Filenames Count = ", len(filenames))
-print(f"{datetime.datetime.now()}: Client - Parsing to files to JSON...")
-#jsons = c.parse_to_json(location_in, filenames, location_out+"_outputs", 10,10,10)
 for f in filenames:
-    print(f"{datetime.datetime.now()}: Client - Sending {f}...")
+    try:
+        os.remove(location_out + f)
+    except:
+        print(c.print_header() + f"File {location_out + f} does not exist.")
+print(c.print_header() + f"Input Location = {location_in}")
+print(c.print_header() + f"Output Location = {location_out}")
+print(c.print_header() + f"Files Count = ", len(filenames))
+filtered_filenames = []
+for f in filenames:
     stats = os.stat(location_in+f)
     if (stats.st_size / (1024 * 1024)) <= size_limit:
-        j = c.json_generate(location_in, f, location_out, brightness, contrast, sharpness)
-        c.send(j)
+        filtered_filenames.append(f)
     else:
-        print(f"{datetime.datetime.now()}: Client - File {f} exceeds the filesize limit of {size_limit}MB!")
-    # response = c.send(j)
-    # if response != None:
-    #     json_body = json.loads(response)
-    #     cv2.imwrite(location_out+"client_"+json_body["filename"], c.json2im(json_body))
+        print(c.print_header() + f"File {f} exceeds the filesize limit of {size_limit}MB!")
+print(c.print_header() + f"Accepted Files Count = ", len(filtered_filenames))
+print(c.print_header() + f"Parsing to files to JSON...")
+#jsons = c.parse_to_json(location_in, filenames, location_out+"_outputs", 10,10,10)
+start = time.time()
+for f in filenames:
+    print(c.print_header() + f"Sending {f}...")
+    repeat = 0
+    while repeat != 10:
+        if c.send(c.json_generate(location_in, f, location_out, brightness, contrast, sharpness)):
+            break
+while True:
+    if len(filtered_filenames) == len(c.get_filenames(location_out)):
+        print(c.print_header() + f"All filtered files received ({len(filtered_filenames)} files)")
+        end = time.time()
+        c.connection_rcv.close()
+        c.connection_snd.close()
+        print(f"Processing Time: {end-start:0.4f}s")
+        exit(0)
--- FinalProj/Server.py
+++ FinalProj/Server.py
@@ -38,49 +38,71 @@ class Server():
         self.finished = Queue()
         self.procs = []
         self.connect_to_server()
-        print(f"{datetime.datetime.now()}: Server - Running {os.cpu_count()} cores...")
+        print(self.print_header() + f"Running {os.cpu_count()} cores...")
         for w in range(os.cpu_count()+1):
             p = Process(target=self.run_queue, args=(int(w),))
             self.procs.append(p)
             p.start()
+        r = Process(target=self.run_deliver, args=())
+        r.start()
         print(f"{datetime.datetime.now()}: Server - Listening...\n")
         try:
-            self.channel.start_consuming()
+            self.channel_rcv.start_consuming()
         except KeyboardInterrupt:
-            self.channel.stop_consuming()
+            self.channel_rcv.stop_consuming()
+
+    def print_header(self):
+        return f'{datetime.datetime.now()}: Server - '
 
     def connect_to_server(self):
         self.credentials = pika.PlainCredentials('rabbituser','rabbit1234')
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(Server.IP, Server.PORT, Server.ROOT, self.credentials, connection_attempts=128, retry_delay=1, heartbeat=600, blocked_connection_timeout=300))
+        self.connection_rcv = pika.BlockingConnection(pika.ConnectionParameters(Server.IP, Server.PORT, Server.ROOT, self.credentials, connection_attempts=128, retry_delay=3, heartbeat=600, blocked_connection_timeout=300))
+        self.connection_snd = pika.BlockingConnection(pika.ConnectionParameters(Server.IP, Server.PORT, Server.ROOT, self.credentials, connection_attempts=128, retry_delay=3, heartbeat=600, blocked_connection_timeout=300))
         print(f"{datetime.datetime.now()}: Server - Credentials -[{self.credentials.username}]:[{self.credentials.password}]")
-        self.channel = self.connection.channel()
-        self.channel.basic_qos(prefetch_count=os.cpu_count())
-        self.channel.exchange_declare(exchange='adjustor', exchange_type=ExchangeType.direct)
-        self.channel.queue_declare(queue='adjustor',durable=True,arguments={'x-max-length':100, 'x-queue-type':'quorum','message-ttl':300000})
-        self.channel.basic_consume(queue='adjustor', on_message_callback=self.on_request, auto_ack=True)
+        self.channel_rcv = self.connection_rcv.channel(1)
+        self.channel_rcv.basic_qos(prefetch_count=os.cpu_count())
+        self.channel_rcv.queue_declare(queue='adjustor', durable=True, arguments={'x-max-length':100, 'x-queue-type':'classic','message-ttl':300000})
+        self.channel_rcv.basic_consume(queue='adjustor', on_message_callback=self.on_request, auto_ack=True)
+        self.channel_snd = self.connection_snd.channel(2)
+        self.channel_snd.basic_qos(prefetch_count=os.cpu_count())
+        self.channel_snd.exchange_declare(exchange='adjustor_fin', exchange_type=ExchangeType.topic)
+        #self.channel_snd.queue_declare(queue='adjustor_fin', durable=True, arguments={'x-max-length':100, 'x-queue-type':'classic','message-ttl':300000})
 
     def run_queue(self, id:int):
         while True:
             try:
                 file = self.pending.get() #This will raise an exception if it is empty
             except queue.Empty: #Excemption raised if queue is empty. Breaks the while loop.
-                print("Queue is Empty!")
+                print("Pending Queue is Empty!")
             else: #No exception has been raised, add the task completion
                 file = json.loads(json.dumps(file))
-                print(f'{datetime.datetime.now()}: Server - Thread {id} Processing {file["filename"]}...')
+                print(self.print_header() + f'Thread {id} Processing {file["filename"]}...')
                 start = time.time()
                 image = self.a.adjust_image(file) #Executes actual image processing
                 end = time.time()
-                print(f'{datetime.datetime.now()}: Server - Thread {id} Processed {file["filename"]} @ {end-start:0.2f}')
+                print(self.print_header() + f'Thread {id} Processed {file["filename"]} @ {end-start:0.2f}s')
                 file["image"] = self.im2json(image)
-                self.finished.put(file)    
+                self.finished.put(file)
+
+    def run_deliver(self):
+        while True:
+            try:
+                file = self.finished.get()
+            except queue.Empty:
+                print("Finished Queue is Empty!")
+            else:
+                file = json.loads(json.dumps(file))
+                json_str = json.dumps(file)
+                print(self.print_header() + f"Returning {file['filename']}...")
+                self.channel_snd.basic_publish(exchange='adjustor_fin', routing_key=file["client_uid"], body=json_str, mandatory=True)
 
     def on_request(self, ch, method, props, body:str):
-        #2 layers of json
         json_body = json.loads(body)
-        self.pending.put(json_body)
+        json_body["client_uid"] = props.headers["client_uid"]
+        json_body["item_uid"] = props.headers["item_uid"]
+        self.pending.put_nowait(json_body)
         print(f"{datetime.datetime.now()}: Server - Queued {json_body['filename']}, Queue = {self.pending.qsize()}")
-        self.connection.process_data_events()
+        self.connection_rcv.process_data_events()
 
     def im2json(self, im):
         """Convert a Numpy array to JSON string"""
